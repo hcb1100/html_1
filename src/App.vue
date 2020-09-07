064b78e6bd84f4c3fcaf66b609fbc76cd92e6120
hcb1100 <h.cb1100y.sr400@gmail.com>
2020-09-07 17:41:56 +0900

    commitment

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="test" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  el: '#list1',
+  data: function (){
+    return{
+    items: [
+      { list: 'ulタグ及びolタグは、liタグを含めることでリスト表現できる。'},
+      { list: 'ulタグはUnorderList、即ち順序・順番に無関係のリストを作成'},
+      { list: 'olタグはOderList、即ち順序・順番に関係あるリストを作成'},
+      { list: 'ul、ol、liには注意がある'}
+    ]}
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <a href="https://www.google.com/webhp?hl=ja&sa=X&ved=0ahUKEwjAnv2c89PrAhXYaN4KHbcnCpYQPAgI">
+        ここをクリックするとgoogle検索Topを開く
+      </a>
+      <p>
+        pタグは段落を表現する時に使用する。<br>
+      <br>
+        brタグを使用することで<br>
+        改行できる。<br>
+        <br>
+        注意点として、brタグは文頭に記載しても動作するが、原則文末に記載した方が良い。<br>
+        文頭に記載しても動作するブラウザもあるかもしれないが、ブラウザは<br>
+        HTML内の改行を半角スぺースに置き換える。<br> 
+        そのためブラウザを表示している横幅次第では予期せぬ箇所で改行が発生してしまう可能性がある。<br>
+        それを防ぐために文末に記載した方が良い。<br>
+        <br>
+      </p>
+      <ul id = "list1">
+        <li v-for = "item in itmes" :key = "item.message">
+          {{ item.message }}
+        </li>
+      </ul>
+      <ol>
+        <li>ulタグ、olタグにはliタグを必ず含める</li><br>
+        <li>逆に、liタグはulタグもしくはolタグで囲う</li><br>
+        <li>従ってそれぞれのタグは単独で使用しない</li><br>
+      </ol>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+  
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
